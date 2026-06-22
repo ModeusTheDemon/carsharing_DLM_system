535cd00c048f1ac6fe6d03dcd1d841b77b741c79
ModeusTheDemon <116836559+ModeusTheDemon@users.noreply.github.com>
2026-06-22 18:52:52 +0200

    Backuper WIP 1
    created all policies and main file

--- python-backup-system/main.py
+++ python-backup-system/main.py
@@ -22,33 +22,33 @@ app.conf.update(
 )
 
 # Обертываем наши функции в задачи Celery
-@app.task(name="tasks.archive_old_rides")
-def task_archive_old_rides():
-    run_archiver()
+@app.task(name="tasks.check_database_health")
+def task_check_database():
+    run_checker()
 
-@app.task(name="tasks.anonymize_users")
-def task_anonymize_users():
-    run_anonymizer()
+@app.task(name="tasks.save_increment")
+def task_save_increment():
+    run_saver_inc()
 
-@app.task(name="tasks.clean_old_archives")
-def task_clean_old_archives():
-    run_cleaner()
+@app.task(name="tasks.save_full")
+def task_save_full():
+    run_saver_full()
 
 # Настраиваем ритмичное расписание (Celery Beat Schedule)
 app.conf.beat_schedule = {
-    # 1. Архивация запускается каждый день в полночь
-    "archive-every-night": {
-        "task": "tasks.archive_old_rides",
-        "schedule": crontab(hour=0, minute=0),
+    # 1. Проверка проводится каждые 5 минут или при уведомлении монитора
+    "check-every-five-minutes": {
+        "task": "tasks.check_database_health",
+        "schedule": crontab(minute="*\5"),
     },
-    # 2. Анонимизация запускается каждый час (на 0-й минуте)
-    "anonymize-every-hour": {
-        "task": "tasks.anonymize_users",
-        "schedule": crontab(minute=0),
+    # 2. бэкап инкремента проводится каждую неделю
+    "save-increment-every-week": {
+        "task": "tasks.save_increment",
+        "schedule": crontab(minute=0, hour=1, day_of_week=1),
     },
-    # 3. Полная очистка древнего архива запускается раз в неделю (в воскресенье в 2 часа ночи)
-    "clean-archives-weekly": {
-        "task": "tasks.clean_old_archives",
-        "schedule": crontab(day_of_week=0, hour=2, minute=0),
+    # 3. бэкап полный каждый месяц
+    "save-full-every-mounth": {
+        "task": "tasks.save_full",
+        "schedule": crontab(),
     },
 }
